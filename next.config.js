cb5189ffddc91bf339397bf91c74657a9d803dea
Supanat Konprom <romeo@Supanats-MacBook-Pro.local>
2021-07-24 21:51:14 +0700

    update

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   reactStrictMode: true,
+  target: 'serverless',
+  exportPathMap: () => ({
+    "/": {
+      page: "/",
+    },
+  }),
 }
