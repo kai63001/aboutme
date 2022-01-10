39d1ce2f1fa2c002fd4a3a20aaa2d32508548e35
kai63001 <animehq5@gmail.com>
2022-01-11 01:01:50 +0700

    update sticky

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,26 +8,28 @@ export default function Home() {
       <div className="parallax">
         <div className="parallax_layer parallax_layer_4 text-center mt-40">
           <h1 className="text-7xl font-mochiy">Hy! I am Supanat</h1>
-          <p className="mt-5 w-3/5">
-            asdas
-          </p>
+          <div className="mt-5 text-center font-mochiy">
+            I code and love doing what I can't. and I like what I do.
+          </div>
         </div>
         <div className="parallax_layer parallax_layer_3">
-          <img src="/images/layer4.png" />
+          <img className="img" src="/images/layer4.png" />
         </div>
         <div className="parallax_layer parallax_layer_2">
-          <img src="/images/layer3.png" />
+          <img className="img" src="/images/layer3.png" />
         </div>
         <div className="parallax_layer parallax_layer_1">
-          <img src="/images/layer2.png" />
+          <img className="img" src="/images/layer2.png" />
         </div>
         <div className="parallax_layer parallax_layer_0">
-          <img src="/images/layer1.png" />
+          <img className="img" src="/images/layer1.png" />
         </div>
         <div className="parallax_cover">
-          <div className="max-w-screen-xl mx-auto mt-3 px-2 xs:px-0">
-            asdasd
-          </div>
+          <nav id="navbar" className=" psitck text-white py-3">
+            <div className="max-w-screen-xl mx-auto mt-3 px-2 xs:px-0">
+              asdasd
+            </div>
+          </nav>
         </div>
       </div>
     </div>
