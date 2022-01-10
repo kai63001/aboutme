bdad4cfed793736be19001e93f54b9b841252c73
kai63001 <animehq5@gmail.com>
2022-01-11 01:38:00 +0700

    update scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useRef } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const about:any = useRef(null)
+  const executeScroll:any = () => about?.current.scrollIntoView({behavior:"smooth"})    
+
   return (
     <div>
       <div className="parallax">
@@ -25,22 +29,29 @@ export default function Home() {
           <img className="img" src="/images/layer1.png" />
         </div>
         <div className="parallax_cover">
-          <nav id="navbar" className=" psitck text-white py-3 bg-main">
+          <nav id="navbar" className="psitck text-white bg-main pb-6 pt-4">
             <div className="max-w-screen-xl mx-auto mt-3 px-2 xs:px-0 flex justify-between">
               <h2>HELLO</h2>
               <ul className="flex space-x-10">
                 <li>
-                  <a href="#">asdasd</a>
+                  <a href="#">Home</a>
                 </li>
                 <li>
-                  <a href="#">asdasd</a>
+                  <button onClick={executeScroll}>About</button>
+                </li>
+                <li>
+                  <button onClick={executeScroll}>Skills</button>
                 </li>
               </ul>
             </div>
           </nav>
           <br />
           <div className="max-w-screen-xl mx-auto mt-3 px-2 xs:px-0 text-white text-lg">
-            asdasdasdasdasdasdasdadasdasdas
+            <div className="text-center">
+              <h2 id="about" ref={about} className="text-3xl">Hi, I’m Supanat Konprom. Nice to meet you.</h2>
+              <p className="mt-3 text-gray-400">Since beginning my journey as a freelance designer nearly 10 years ago, I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
+              </p>
+            </div>
           </div>
         </div>
       </div>
