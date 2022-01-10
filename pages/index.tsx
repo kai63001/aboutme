ace5ab753ddb9ecad08985ba5158edb2139872cf
kai63001 <animehq5@gmail.com>
2022-01-10 22:37:20 +0700

    add image parallax

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,23 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <div className="text-red-600">asdasd</div>
+      <div className="parallax">
+        <div className="parallax__layer parallax__layer__3">
+          <img src="/images/layer4.png" />
+        </div>
+        <div className="parallax__layer parallax__layer__2">
+          <img src="/images/layer3.png" />
+        </div>
+        <div className="parallax__layer parallax__layer__1">
+          <img src="/images/layer2.png" />
+        </div>
+        <div className="parallax__layer parallax__layer__0">
+          <img src="/images/layer1.png" />
+        </div>
+        <div className="parallax__cover">
+          asdasdasd
+        </div>
+      </div>
     </div>
   )
 }
