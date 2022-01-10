6acb16dbb8bdc83c1b234cae5bed285e03652da0
kai63001 <animehq5@Gmail.com>
2022-01-10 16:10:29 +0000

    Deploying to gh-pages from  @ eed2c4b84d8e6cbeb1d95ba8890bd0678a865439 🚀

---
+++ _next/static/FZUxrMJlw7c54TVJ_MudA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a23a6fe6c1c5c7eaf08c.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
