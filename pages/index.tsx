fb4dc30698caf537ea6337355d577976d6ee7e4e
kai63001 <animehq5@gmail.com>
2022-01-11 17:46:30 +0700

    update scroll to element and fix prallax shit

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,29 +7,27 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import LessSkills from '@/components/skils/less'
+import useOnScreen from '@/lib/useOnScreen'
+
 
 export default function Home() {
   const [scrollY ,setScrollY] = useState(0);
+  const page: any = useRef(null)
+
 
   //skills 
   const skills: any = useRef(null)
   const skillsScroll = () => skills?.current.scrollIntoView({ behavior: "smooth" })
-
+  const skillisVisible = useOnScreen(skills)
   //about
   const about: any = useRef(null)
   const aboutScroll: any = () => about?.current.scrollIntoView({ behavior: "smooth" })
+  const aboutisVisible = useOnScreen(about)
 
   library.add(fab, fas)
-  const handleScroll = (e:any) => {
-    setScrollY(window.scrollY - window.screen.height + 286)
-  }
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener("scroll", handleScroll)
-    }
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [handleScroll])
+
+
 
   return (
     <div>
@@ -52,16 +50,16 @@ export default function Home() {
         <div className="parallax_layer parallax_layer_0">
           <img className="img" src="/images/layer1.png" />
         </div>
-        <div className="parallax_cover">
+        <div className="parallax_cover z-100 overflow-x-visible" ref={page}>
           <nav id="navbar" className="psitck text-white bg-main pb-6 pt-4 z-100">
             <div className="max-w-screen-xl mx-auto mt-3 px-2 xs:px-0 flex justify-between">
-              <h2>HELLO</h2>
+              <h2 onClick={()=> console.log('')}>HELLO</h2>
               <ul className="flex space-x-5">
                 <li>
-                  <button className={`hover:bg-purple-800 ${scrollY >= about?.current?.offsetTop && scrollY < skills.current?.offsetTop ? 'bg-purple-800':''} rounded-md px-4 py-2`} onClick={aboutScroll}>About</button>
+                  <button className={`hover:bg-purple-800 ${aboutisVisible && 'bg-purple-800'} rounded-md px-4 py-2`} onClick={aboutScroll}>About</button>
                 </li>
                 <li>
-                  <button className={`hover:bg-purple-800 ${scrollY >= skills.current?.offsetTop ? 'bg-purple-800':''} rounded-md px-4 py-2`} onClick={skillsScroll}>Skills</button>
+                  <button className={`hover:bg-purple-800 ${ !aboutisVisible && skillisVisible && 'bg-purple-800'} rounded-md px-4 py-2`} onClick={skillsScroll}>Skills</button>
                 </li>
                 <li>
                   <button className={`hover:bg-purple-800 rounded-md px-4 py-2`} onClick={skillsScroll}>Open source</button>
@@ -75,10 +73,10 @@ export default function Home() {
               <h2 id="about" ref={about} className="text-3xl">Hi, I’m Supanat Konprom. Nice to meet you.</h2>
               <p className="mt-3 text-gray-400 w-3/4 m-auto">Student Software Engineer 🚀 Experience building web, mobile applications and games with other cool libraries and frameworks.</p>
             </div>
-            <div className="grid sm:grid-cols-7 grid-cols-1 gap-4 mt-10">
+            <div ref={skills} id="skills" className="grid sm:grid-cols-7 grid-cols-1 gap-4 mt-10">
               <div className="col-span-3 sm:order-1 order-2"><img src="/images/whatido.png" alt="" /></div>
-              <div className="sm:order-2 col-span-4 order-1">
-                <h2 id="skills" className="text-3xl" ref={skills}>What i do</h2>
+              <div  className="sm:order-2 col-span-4 order-1">
+                <h2 className="text-3xl" >What i do</h2>
                 <LessSkills />
                 <div className="text-center mt-5">
                   <button className="bg-orange-600 px-5 py-2 rounded-lg focus:outline outline-offset-1 outline-orange-900"><FontAwesomeIcon className='mr-2 down-animation' size="lg" icon={['fas', 'arrow-down']} /> Show more</button>
