491485f3135cb34dacf65ff3432c18dcba32247d
kai63001 <animehq5@gmail.com>
2022-01-16 01:10:15 +0700

    add more porject

--- pages/index.tsx
+++ pages/index.tsx
@@ -217,6 +217,16 @@ export default function Home() {
                 />
               ))}
             </div>
+            <div className="text-center mt-6">
+              <Link href="https://github.com/kai63001">
+                <a
+                  target={"_blank"}
+                  className="bg-purple-600 px-5 py-3 rounded-lg focus:outline outline-offset-1 outline-purple-900"
+                >
+                  More Porjects
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
